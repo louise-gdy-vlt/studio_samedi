b39cc17720c8766613fc0f299317118c53d8bb8d
Louise Geday-Volait <louise@Louises-MacBook-Pro.local>
2018-04-06 16:18:03 +0200

    v1.06 débug svg + anomalie

--- js/navbar.js
+++ js/navbar.js
@@ -1,5 +1,5 @@
 var c =document.getElementById("collapse");
-var t =document.getElementById("toggle");
+var t =document.getElementById("hamburger");
 
 t.addEventListener("click", function(){
     if (c.classList.contains("on")) {
